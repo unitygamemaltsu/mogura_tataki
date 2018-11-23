d48a021fd1e07003e43623aace4f4cf4c3ca813e
tsubasasabust1 <keytsubasa823@gmail.com>
2018-11-24 07:05:55 +0900

    bgmとスコア変えた

--- Assets/Scripts/BGM.cs
+++ Assets/Scripts/BGM.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class BGM : MonoBehaviour {
     public AudioClip[] clips;
     AudioSource audio;
+
+	private 
 	// Use this for initialization
 	void Start () {
+        
 		audio = GetComponent<AudioSource>();
+		//audio.clip = clips[0];
+        //audio.Play();
+		if(StartGame.getLebel() == 1){
 		audio.clip = clips[0];
         audio.Play();
+		}
+		if(StartGame.getLebel() == 2){
+		audio.clip = clips[1];
+        audio.Play();
+		}
+		if(StartGame.getLebel() == 3){
+		audio.clip = clips[2];
+        audio.Play();
+		}
+		if(StartGame.getLebel() == 4){
+		audio.clip = clips[2];
+        audio.Play();
+		}
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
     public Text TimerText;
     private float timer;
     int seconds;
 
-    
+
     //private float totalTime;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         //StartReset();
-        //timer = 30.0f;
-        timer = 5.0f;//debugmode
+        timer = 30.0f;
+        //timer = 5.0f;//debugmode
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         timer -= Time.deltaTime;
         seconds = (int)timer;
         TimerText.text = seconds.ToString();
-        if(seconds <= 0){
+        if (seconds <= 0)
+        {
             int lebel = StartGame.getLebel();
             Debug.Log("レベル: " + lebel);
             if (StartGame.upScores[lebel - 1] <= Score.Instance.score)
@@ -40,6 +44,53 @@ public class Timer : MonoBehaviour {
             // }
 
             SceneManager.LoadScene("ResultScene");
+
+            if (StartGame.getLebel() == 1)//多分レベルアップ後値が参照されているつまりこれ意味なし
+            {
+                if (Score.Instance.score <= 10)
+                {
+                    SceneManager.LoadScene("ResultScene2");
+                }
+                else
+                {
+                    SceneManager.LoadScene("ResultScene");
+                }
+            }
+
+            else if (StartGame.getLebel() == 2)
+            {
+                if (Score.Instance.score <= 20)
+                {
+                    SceneManager.LoadScene("ResultScene2");
+                }
+                else
+                {
+                    SceneManager.LoadScene("ResultScene");
+                }
+            }
+
+            else if (StartGame.getLebel() == 3)
+            {
+                if (Score.Instance.score <=30 )
+                {
+                    SceneManager.LoadScene("ResultScene2");
+                }
+                else
+                {
+                    SceneManager.LoadScene("ResultScene");
+                }
+            }
+            else if (StartGame.getLebel() == 4)
+            {
+                if (Score.Instance.score <= 35 )
+                {
+                    SceneManager.LoadScene("ResultScene2");
+                }
+                else
+                {
+                    SceneManager.LoadScene("ResultScene3");
+                }
+            }
         }
     }
 
